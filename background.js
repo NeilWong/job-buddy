1be0016dc100ff1d7e1328a660214809fc27552f
rvdang <rvdang98@gmail.com>
2019-02-16 17:42:34 -0800

    feat: gets job information

--- background.js
+++ background.js
@@ -1,3 +1,16 @@
+jQuery.expr[':'].regex = function(elem, index, match) {
+    var matchParams = match[3].split(','),
+        validLabels = /^(data|css):/,
+        attr = {
+            method: matchParams[0].match(validLabels) ? 
+                        matchParams[0].split(':')[0] : 'attr',
+            property: matchParams.shift().replace(validLabels,'')
+        },
+        regexFlags = 'ig',
+        regex = new RegExp(matchParams.join('').replace(/^\s+|\s+$/g,''), regexFlags);
+    return regex.test(jQuery(elem)[attr.method](attr.property));
+}
+
 $(document).on({
     mouseenter: function () {
         var $this = $(this);
@@ -14,6 +27,23 @@ $(document).on({
 $(document).on("click", "button.jobs-apply-button", function() {
     
     // get job info
+    const $jobTitle = $("h1:regex(class, jobs(-details)?-top-card__job-title)")
+    const $companyName = $("a:regex(class, jobs(-details)?-top-card__company-url)")
+    let $companyLocation = $("a:regex(class, jobs(-details)?-top-card__exact-location)")
+    if ($companyLocation.text() === ""){
+        $companyLocation = $("span:regex(class, jobs(-details)?-top-card__bullet)").first()
+    }
+    const $jobDescription = $("div.jobs-description-content__text")
+
+    jobTitle = $jobTitle.text().trim()
+    companyName = $companyName.text().trim()
+    companyLocation = $companyLocation.text().trim()
+    jobDescription = $jobDescription.text().trim()
+
+    console.log(jobTitle)
+    console.log(companyName)
+    console.log(companyLocation)
+    console.log(jobDescription)
     
     alert("The relevant information about this job listing was saved in your spreadsheet!")
 });
