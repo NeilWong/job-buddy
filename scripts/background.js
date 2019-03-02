24bcec94ea3d2ea44ac9d6db2b86d7b84a39d62d
rvdang <rvdang98@gmail.com>
2019-03-02 08:15:48 -0800

    fixes firebase permissions and tests addNewJob function

--- scripts/background.js
+++ scripts/background.js
@@ -5,7 +5,7 @@ var config = {
     projectId: "job-buddy-1",
     storageBucket: "job-buddy-1.appspot.com",
     messagingSenderId: "1065356240334"
-  };
+};
 firebase.initializeApp(config);
 var database = firebase.database();
 
@@ -41,7 +41,7 @@ function addNewJob(job) {
 
     var newJob = {
         companyId,
-        website: 'linkedIn',
+        website: 'LinkedIn',
         title: job.jobTitle,
         location: job.companyLocation,
         // url
@@ -62,127 +62,4 @@ function addNewJob(job) {
     return database.ref().update(updates)
 }
 
-jQuery.expr[':'].regex = function(elem, index, match) {
-    var matchParams = match[3].split(','),
-        validLabels = /^(data|css):/,
-        attr = {
-            method: matchParams[0].match(validLabels) ? 
-                        matchParams[0].split(':')[0] : 'attr',
-            property: matchParams.shift().replace(validLabels,'')
-        },
-        regexFlags = 'ig',
-        regex = new RegExp(matchParams.join('').replace(/^\s+|\s+$/g,''), regexFlags);
-    return regex.test(jQuery(elem)[attr.method](attr.property));
-}
-
-$(document).on({
-    mouseenter: function () {
-        var $this = $(this);
-        $this.html("<img style='height:20px;width:auto;' src='" + chrome.extension.getURL('images/icon-full.png') + "'>" + "&nbsp;&nbsp;" + "Save");
-    },
-    mouseleave: function () {
-        var $this = $(this);
-        $this.html("Save");
-    }
-}, "button.jobs-save-button > button.artdeco-button");
-
-$(document).on("click", "button.jobs-save-button", function() {
-    // get job info
-    const $jobTitle = $("h1:regex(class, jobs(-details)?-top-card__job-title)")
-    const $companyName = $("a:regex(class, jobs(-details)?-top-card__company-url)")
-    let $companyLocation = $("a:regex(class, jobs(-details)?-top-card__exact-location)")
-    if ($companyLocation.text() === ""){
-        $companyLocation = $("span:regex(class, jobs(-details)?-top-card__bullet)").first()
-    }
-    const $jobDescription = $("div.jobs-description-content__text > span")
-
-    jobTitle = $jobTitle.text().trim()
-    companyName = $companyName.text().trim()
-    companyLocation = $companyLocation.text().trim()
-    jobDescription = $jobDescription.html().trim()
-
-    let job = {};
-    job = {
-        jobTitle,
-        companyName,
-        companyLocation,
-        status: "Saved",
-        dateApplied: new Date(Date.now()).toDateString(),
-        jobDescription,
-    }
-
-    addNewJob(job)
-
-    chrome.storage.local.get({lastId: 0}, function(data){
-        job['lastId'] = data.lastId
-        chrome.storage.local.set({
-            lastId: data.lastId + 1
-        })
-    })
-
-    chrome.storage.local.get({jobs: []}, function(data) {
-        update(data.jobs, job);
-    });  
-});
-
-$(document).on({
-    mouseenter: function () {
-        var $this = $(this);
-        $this.data('bgcolor', $this.css('background-color')).css('background-color', '#3c9963');
-        $this.html("<img style='height:20px;width:auto;' src='" + chrome.extension.getURL('images/icon-full.png') + "'>" + "&nbsp;&nbsp;" + "Apply");
-    },
-    mouseleave: function () {
-        var $this = $(this);
-        $this.css('background-color', $this.data('bgcolor'));
-        $this.html("Apply");
-    }
-}, "button.jobs-apply-button");
-
-$(document).on("click", "button.jobs-apply-button", function() {
-    // get job info
-    const $jobTitle = $("h1:regex(class, jobs(-details)?-top-card__job-title)")
-    const $companyName = $("a:regex(class, jobs(-details)?-top-card__company-url)")
-    let $companyLocation = $("a:regex(class, jobs(-details)?-top-card__exact-location)")
-    if ($companyLocation.text() === ""){
-        $companyLocation = $("span:regex(class, jobs(-details)?-top-card__bullet)").first()
-    }
-    const $jobDescription = $("div.jobs-description-content__text > span")
-
-    jobTitle = $jobTitle.text().trim()
-    companyName = $companyName.text().trim()
-    companyLocation = $companyLocation.text().trim()
-    jobDescription = $jobDescription.html().trim()
-
-    let job = {};
-    job = {
-        jobTitle,
-        companyName,
-        companyLocation,
-        status: "Applied",
-        dateApplied: new Date(Date.now()).toDateString(),
-        jobDescription,
-    }
-
-    chrome.storage.local.get({lastId: 0}, function(data){
-        job['lastId'] = data.lastId
-
-        chrome.storage.local.set({
-            lastId: data.lastId + 1
-        })
-    })
-
-    chrome.storage.local.get({jobs: []}, function(data) {
-        update(data.jobs, job);
-    });  
-});
-
-function update(array, job) {
-    array.push(job);
-    chrome.storage.local.set({
-        jobs: array
-    }, function() {
-        chrome.storage.local.get(['jobs'], function(data) {
-            console.log(data.jobs);
-        })
-    });
-}
+// addNewJob({companyName: 'Google Chrome', jobTitle: 'Software Developer', companyLocation: 'California', dateCreated: new Date().toDateString(), dateApplied: new Date().toDateString(), status: 'applied'})
